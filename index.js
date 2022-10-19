6d088dd8d5a1137a4a2eec1c675efaaf16b35601
Bryan Miranda <bryan.miranda@sofka.com.co>
2022-10-19 12:01:31 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,72 @@
+
+
+// alert("Hola Bryan (desde archivo index.js)")      //comentario de lina
+
+
+/*
+Comentario en bloque 
+*/
+
+
+var miEdad = 34;
+var nombre = "Bryan";
+var pi = 3.25;
+
+var _correo;
+var $direccion;
+
+var mi_celular;
+
+
+//constante
+
+const mes = 10;
+
+
+//operadores aritmeticos
+
+
+var numeroUno= 10;
+var numeroDos = 20;
+
+console.log(numeroUno + numeroDos);
+console.log(numeroUno - numeroDos);
+console.log(numeroUno * numeroDos);
+console.log(numeroUno ** numeroDos);
+console.log(numeroUno / numeroDos);
+console.log(numeroUno % numeroDos);
+
+
+// Cadenas de texto;
+
+var nombres = "Bryan Alexander";
+let apellidos = 'Miranda Bolaños';
+console.log(nombres);
+console.log(apellidos);
+
+
+
+//Booleanos
+
+
+var bandera = true;
+var otraBandera = false;
+
+
+//undefined
+
+var correoElectronico;
+console.log(correoElectronico);
+
+//concatenacion
+
+console.log(nombres + " " + apellidos)
+
+
+//incrmento y decrmento
+
+numeroUno++;
+numeroUno++;
+numeroUno--;
+
+console.log(numeroUno);
